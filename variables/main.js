4f22f60218cb55443eda3065398ceb23aa58fb87
Ivan Kravtsov <cold67@mail.ru>
2019-03-23 09:32:45 +0800

    Homework variables uploaded

---
+++ variables/main.js
@@ -0,0 +1,27 @@
+// Первое Задание
+
+let x = 2,
+    y = 22,
+    z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+// Второе Задание
+
+let algebra = 3,
+    geography = 2,
+    physics = 4;
+
+const average = (algebra + geography + physics) / 3;
+
+console.log(average);
+
+// Последнее Задание
+
+let name = "Иван";
+
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
