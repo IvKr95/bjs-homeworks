cb7345455ef76e0768cdc904cd482ea6b39712c4
Ivan Kravtsov <cold67@mail.ru>
2019-03-28 18:28:44 +0800

    First commit Extended Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -12,7 +12,22 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    //return x;
+    let d = b**2 - 4*a*c;
+    
+    if(d < 0) {
+        console.log("Корней нет");
+        return "Корней нет";
+    } else if(d === 0) {
+        let x = (-b) / (2*a);
+        console.log(x);
+        return x;
+    } else if(d > 0) {
+        let x1 = ((-b + Math.sqrt(d)) / (2*a)).toFixed(2),
+            x2 = ((-b - Math.sqrt(d)) / (2*a)).toFixed(2);
+
+        console.log([x1, x2]);
+        return [x1, x2];
+    }
 }
 
 function calculateDrinkTask(){
@@ -24,8 +39,18 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+    let currentDate = new Date();
+    let userAge = currentDate.getFullYear() - dateOfBirthday.getFullYear();
+
+    if (userAge >= 18) {
+        console.log(`Не желаете ли олд-фэшн, ${name}?`);
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. 
+                    Зато могу предложить вам замечательный клюквенный компот!`);
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. 
+                Зато могу предложить вам замечательный клюквенный компот!`;
+    };
 }
 
 function calculateAverageRating(){
@@ -36,5 +61,19 @@ function calculateAverageRating(){
 
 function getAverageMark(marks){
     // код для задачи №3 писать здесь
-    //return averageMark;
+    let counter = 0,
+        averageMark;
+
+    if(marks.length > 5) {
+        console.log(`Kоличество оценок больше 5`);
+        marks.splice(5);
+    };
+    
+    for(let mark of marks) {
+        counter += mark;
+    };
+
+    averageMark = counter / marks.length;
+
+    return averageMark;
 }
